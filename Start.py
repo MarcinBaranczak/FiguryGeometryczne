1df98b5cc53f7091471e88db352f896e87555612
Marcin Barańczak <46418050+MarcinBaranczak@users.noreply.github.com>
2019-01-11 09:26:30 +0100

    ver  0.2

--- Start.py
+++ Start.py
@@ -1,3 +1,8 @@
+# ----- pobieranie danych -----
+def pobierzDane(info):
+    return int(input(info))
+
+
 # ----- rysuje kwadrat o zadanym boku -----
 def kwadrat(bok):
     for i in range(bok):
@@ -23,13 +28,13 @@ def kwadrat(bok):
 def romb(wysokosc):
     for j in range(wysokosc):
 
-        for i in range(wysokosc-j):
+        for i in range(wysokosc - j):
             print(" ", end="")
         # endFor
 
         print("/", end="")
 
-        for i in range(j*2):
+        for i in range(j * 2):
             print(" ", end="")
         # enFor
 
@@ -38,13 +43,13 @@ def romb(wysokosc):
 
     for j in range(wysokosc):
 
-        for i in range(j+1):
+        for i in range(j + 1):
             print(" ", end="")
         # endFor
 
         print("\\", end="")
 
-        for i in range(2*(wysokosc-j-1),0,-1):
+        for i in range(2 * (wysokosc - j - 1), 0, -1):
             print(" ", end="")
         # enFor
 
@@ -52,11 +57,42 @@ def romb(wysokosc):
     # endFor
 
 
-# ----- pobieranie danych -----
-def pobierzDane():
-    return int(input("podaj x: "))
+# ----- rysuje trójkąt prostokątny, równoramienny o zadanym boku -----
+def trojkat(bok):
+    print("*")
+
+    for i in range(bok-2):
+        print("*", end="")
+
+        for j in range(i):
+            print(" ", end="")
+        # endFor
+
+        print("*")
+    # endFor
+
+    for i in range (bok):
+        print("*", end="")
+    # endFor
+
+
+# ----- menu -----
+def menu():
+    print("Wybierz figure: ")
+    print("1: Kwadrat")
+    print("2: Trojkat")
+    print("3: Romb")
+
+    wybor = pobierzDane("Twoj wybor: ")
+    bok = pobierzDane("Podaj wielkosc boku: ")
+
+    if wybor == 1:
+        kwadrat(bok)
+    if wybor == 2:
+        trojkat(bok)
+    if wybor == 3:
+        romb(bok)
 
 
 # ----- program głóny -----
-
-romb(pobierzDane())
+menu()
